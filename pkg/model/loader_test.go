856cf9ef7bef40a3b53761debc2b9374cf56cda4
majiayu000 <1835304752@qq.com>
2025-12-29 00:09:02 +0800

    fix: filter out GGUF files from /models endpoint
    Add .gguf extension to the knownModelsNameSuffixToSkip list to prevent
    GGUF model data files from appearing in the /models API response. GGUF
    files are binary model weights that should not be listed as standalone
    models in the API.
    
    Fixes #1077
    
    Signed-off-by: majiayu000 <1835304752@qq.com>

--- pkg/model/loader_test.go
+++ pkg/model/loader_test.go
@@ -68,6 +68,18 @@ var _ = Describe("ModelLoader", func() {
 			Expect(files).To(ContainElement("test.model"))
 			Expect(files).ToNot(ContainElement("README.md"))
 		})
+
+		It("should filter out GGUF files from the model list", func() {
+			os.Create(filepath.Join(modelPath, "valid-model"))
+			os.Create(filepath.Join(modelPath, "model.gguf"))
+			os.Create(filepath.Join(modelPath, "another-model.gguf"))
+
+			files, err := modelLoader.ListFilesInModelPath()
+			Expect(err).To(BeNil())
+			Expect(files).To(ContainElement("valid-model"))
+			Expect(files).ToNot(ContainElement("model.gguf"))
+			Expect(files).ToNot(ContainElement("another-model.gguf"))
+		})
 	})
 
 	Context("LoadModel", func() {
